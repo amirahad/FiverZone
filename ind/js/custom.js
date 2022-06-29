cb1424e056509ad4d34007f82e09a7b39a5b723a
amirahad <arahad4783@gmail.com>
2022-06-30 04:19:22 +0600

    home page final

--- ind/js/custom.js
+++ ind/js/custom.js
@@ -79,6 +79,13 @@ $(function () {
     var $nav = $(".header_section");
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height()*2);
     // console.log("applied");
+    var $anchor = $('.any');
+    if($(this).scrollTop() > $nav.height()*2){
+      $anchor.css('color', '#1A78F2');
+    }else{
+      $anchor.css('color', '#fff');
+    }
+    // $anchor.toggleClass('blue', $(this).scrollTop() > $nav.height()*2);
     var $nav2 = $(".high");
     // console.log($nav2.height());
     $nav2.toggleClass('d-md-block', $(this).scrollTop() > $nav.height()*4)
@@ -96,4 +103,31 @@ $(function () {
     }
     // $logo.attr("src", "./images/logo5.png", $(this).scrollTop() > $nav.height()*2);
   });
+});
+
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: false,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  autoplay:{
+    delay: 2000,
+  },
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    // },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+    loop: true,
 });
