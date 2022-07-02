c6c0f222a494c60be28df7af319afd29bb63cda1
amirahad <arahad4783@gmail.com>
2022-07-02 17:08:35 +0600

    details page final

--- ind/js/custom.js
+++ ind/js/custom.js
@@ -1,77 +1,4 @@
-$(document).ready(function(){
-	// $('.main_sliders').slick({
-  //       dots: false,
-  //       arrows: true,
-  //       infinite: true, 
-	// 	autoplay: true,
-	// 	autoplaySpeed: 10000,
-  //       speed: 3000, 
-  //       nextArrow:'.slidNext',
-  //       prevArrow:'.slidPrv',
-  //       slidesToShow: 1,
-  //       slidesToScroll: 1,
-  //       responsive: [
-  //         {
-  //           breakpoint: 1024,
-  //           settings: {
-  //             slidesToShow:1,
-  //             slidesToScroll: 1,
-  //             infinite: true,
-  //             dots: false,
-  //       arrows: true,
-  //           }
-  //         },
-  //         {
-  //           breakpoint: 600,
-  //           settings: {
-  //             slidesToShow: 1,
-	// 		  speed: 1000,
-  //             slidesToScroll: 2
-  //           }
-  //         },
-  //         {
-  //           breakpoint: 480,
-  //           settings: {
-  //             slidesToShow: 1,
-	// 		  speed: 1000, 
-  //             slidesToScroll: 1
-  //           }
-  //         } 
-  //       ]
-  //     });
-  
-  $('.slider').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows: false,
-    responsive: [
-      {
-        breakpoint: 769,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
 
-  
-});
 
 
 $(function () {
@@ -130,4 +57,46 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
     loop: true,
+});
+
+
+var swiper2 = new Swiper(".mySwiper2", {
+  effect: "coverflow",
+  grabCursor: false,
+  centerPadding: "60px",
+  autoplay: true,
+  autoplaySpeed: 2000,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  responsive: [
+    {
+      breakpoint: 769,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+  ],
 });
